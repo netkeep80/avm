8abee025d581863af7b1a5f94fdbccd862c5af2c
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 02:28:09 +0300

    AVM 1.0: replace legacy interpreter with projection facade

---
+++ test/legacy_facade_test.cpp
@@ -0,0 +1,59 @@
+#include "avm/legacy_json_compat.h"
+
+#include <cassert>
+
+namespace
+{
+
+json interpret_and_export(const json &expression)
+{
+	rel_t *result = interpret(expression);
+	json projected;
+	export_json(result, projected);
+	return projected;
+}
+
+} // namespace
+
+int main()
+{
+	clear_func_env();
+	assert(interpret(json(true)) == rel_t::True);
+	assert(interpret(json(false)) == rel_t::False);
+	assert(interpret(json(nullptr)) == rel_t::E);
+	assert(interpret_and_export(json{{"Not", json::array({false})}}) == json(true));
+
+	clear_func_env();
+	const json definition = {{"Def", json::array({"id", json::array({"x"}), "x"})}};
+	assert(interpret(definition) == rel_t::E);
+	assert(interpret_and_export(json{{"Call", json::array({"id", 42})}}) == json(42));
+	assert(interpret_and_export(json{{"Call", json::array({"id", "hello"})}}) == json("hello"));
+
+	clear_func_env();
+	assert(interpret(json{{"Call", json::array({"id", true})}}) == rel_t::E);
+
+	clear_func_env();
+	const json recursive_program = json::array({
+	    json{{"Def",
+	          json::array({"recur", json::array({"flag"}),
+	                       json{{"If",
+	                             json::array({"flag", json{{"Call", json::array({"recur", false})}}, true})}}})}},
+	    json{{"Call", json::array({"recur", true})}},
+	});
+	assert(interpret(recursive_program) == rel_t::True);
+
+	clear_func_env();
+	const json continue_after_error = json::array({
+	    json{{"Call", json::array({"later", true})}},
+	    json{{"Def", json::array({"later", json::array({"x"}), "x"})}},
+	    json{{"Call", json::array({"later", false})}},
+	});
+	assert(interpret(continue_after_error) == rel_t::False);
+
+	clear_func_env();
+	const json lazy = {
+	    {"If", json::array({true, true, json{{"Call", json::array({"missing"})}}})}};
+	assert(interpret(lazy) == rel_t::True);
+
+	return 0;
+}
