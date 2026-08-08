d06c48a0120dee130df43e593d9a95ace19d8863
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 08:29:34 +0300

    release: set public version to 1.0.0

--- include/avm/version.h
+++ include/avm/version.h
@@ -5,9 +5,9 @@
 namespace avm
 {
 
-inline constexpr int version_major = 0;
+inline constexpr int version_major = 1;
 inline constexpr int version_minor = 0;
-inline constexpr int version_patch = 5;
-inline constexpr std::string_view version_string = "0.0.5";
+inline constexpr int version_patch = 0;
+inline constexpr std::string_view version_string = "1.0.0";
 
 } // namespace avm
