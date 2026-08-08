a896bd2a7d618d11c4da4e86a4bc597f8fb7acc8
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 13:15:52 +0300

    version: start AVM 1.2.0 structural library

--- include/avm/version.h
+++ include/avm/version.h
@@ -6,8 +6,8 @@ namespace avm
 {
 
 inline constexpr int version_major = 1;
-inline constexpr int version_minor = 1;
+inline constexpr int version_minor = 2;
 inline constexpr int version_patch = 0;
-inline constexpr std::string_view version_string = "1.1.0";
+inline constexpr std::string_view version_string = "1.2.0";
 
 } // namespace avm
