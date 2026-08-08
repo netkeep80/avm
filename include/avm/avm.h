1eb90def49d0980d39936dbc95983dc8d5090ee8
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 08:24:19 +0300

    AVM 1.0: add supported core umbrella header

---
+++ include/avm/avm.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "avm/bootstrap_runtime.h"
+#include "avm/executor.h"
+#include "avm/link_store.h"
+#include "avm/persistent_link_store.h"
+#include "avm/program_model.h"
+#include "avm/projection.h"
+#include "avm/raw_carrier.h"
+#include "avm/relations_model.h"
+#include "avm/version.h"
