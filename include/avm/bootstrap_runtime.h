c8395790f2dd4cf5db66130f887b5b44cfa7bfc7
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 14:58:05 +0300

    Match runtime handler formatting

--- include/avm/bootstrap_runtime.h
+++ include/avm/bootstrap_runtime.h
@@ -318,7 +318,8 @@ private:
 		                          [this](const ExecutionContext &context, Executor &executor)
 		                          {
 			                          const std::vector<LinkId> arguments = expression_arguments(context, 1);
-			                          const LinkId value = executor.execute(arguments[0], context.entity, context.frame);
+			                          const LinkId value =
+			                              executor.execute(arguments[0], context.entity, context.frame);
 			                          return store_.get(value).begin;
 		                          });
 
@@ -326,7 +327,8 @@ private:
 		                          [this](const ExecutionContext &context, Executor &executor)
 		                          {
 			                          const std::vector<LinkId> arguments = expression_arguments(context, 1);
-			                          const LinkId value = executor.execute(arguments[0], context.entity, context.frame);
+			                          const LinkId value =
+			                              executor.execute(arguments[0], context.entity, context.frame);
 			                          return store_.get(value).end;
 		                          });
 
@@ -335,7 +337,8 @@ private:
 		                          {
 			                          const std::vector<LinkId> arguments = expression_arguments(context, 2);
 			                          const LinkId left = executor.execute(arguments[0], context.entity, context.frame);
-			                          const LinkId right = executor.execute(arguments[1], context.entity, context.frame);
+			                          const LinkId right =
+			                              executor.execute(arguments[1], context.entity, context.frame);
 			                          return left == right ? vocabulary_.true_value : vocabulary_.false_value;
 		                          });
 
@@ -343,7 +346,8 @@ private:
 		                          [this](const ExecutionContext &context, Executor &executor)
 		                          {
 			                          const std::vector<LinkId> arguments = expression_arguments(context, 2);
-			                          const LinkId begin = executor.execute(arguments[0], context.entity, context.frame);
+			                          const LinkId begin =
+			                              executor.execute(arguments[0], context.entity, context.frame);
 			                          const LinkId end = executor.execute(arguments[1], context.entity, context.frame);
 			                          return store_.find(begin, end) ? vocabulary_.true_value : vocabulary_.false_value;
 		                          });
