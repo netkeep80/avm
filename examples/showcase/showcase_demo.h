48be337de836a82f5024de62d470778c01999fa8
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-15 00:25:45 +0300

    Read showcase screenshot path portably

--- examples/showcase/showcase_demo.h
+++ examples/showcase/showcase_demo.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <optional>
 #include <string>
 
 namespace avm::showcase
 {
 
+std::optional<std::string> framebuffer_capture_path_from_environment();
 void write_framebuffer_ppm(const std::string &path, int width, int height);
 
 } // namespace avm::showcase
