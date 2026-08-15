806d4d014acd3d2ef89c8cb3807bc2ce3b95e46e
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-15 09:36:52 +0300

    Align Native JSON symbol table formatting

--- examples/showcase/native_json_view.cpp
+++ examples/showcase/native_json_view.cpp
@@ -28,13 +28,14 @@ constexpr const char *integer_add_source = R"json({
 
 json_duplet::SymbolAnchors native_symbols(const BootstrapVocabulary &bootstrap, const IntegerVocabulary &integers)
 {
-	return {
+	json_duplet::SymbolAnchors symbols{
 	    {"false", bootstrap.false_value},
 	    {"integer_add", integers.add_relation},
 	    {"nil", bootstrap.nil},
 	    {"true", bootstrap.true_value},
 	    {"unit", bootstrap.unit},
 	};
+	return symbols;
 }
 
 } // namespace
